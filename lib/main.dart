f803d40fdda0cabedf14035ac84c9866e5ffaaed
Jason Shuvo <AJasonShuvo@gmail.com>
2021-08-02 11:22:13 +0600

    Cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-
+import 'package:ecomer/Extra/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:ecomer/checkout.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:ecomer/productProvider.dart';
+import 'package:ecomer/Extra/login.dart';
+import 'package:ecomer/Extra/category_provider.dart';
+import 'package:ecomer/cartpage.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,13 +21,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider<CategoryProvider>(
+          create: (context) => CategoryProvider(),
+        ),
         ChangeNotifierProvider<ProductProvider>(
           create: (context) => ProductProvider(),
         ),
       ],
       child: MaterialApp(
-
-        home: CheckOut()
+        theme: ThemeData(
+          primaryColor: Color(0xff746bc9),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return cartpage();
+            } else {
+              return Login();
+            }
+          },
+        ),
       ),
     );
   }
